0bbb00c1a1edfbc5cf93f22ea660d84e924ec116
Vlad Bloshenko <71842735+blotenko@users.noreply.github.com>
2021-12-01 11:07:03 +0200

    Add files via upload
    Uploading swift project

---
+++ ContentView.swift
@@ -0,0 +1,38 @@
+
+import SwiftUI
+
+struct ContentView : View {
+    var tutors: [Tutor] = []
+    
+    var body: some View {
+        NavigationView {
+            List(tutors) { tutor in
+                TutorCell(tutor: tutor)
+            }.navigationBarTitle(Text("Tutors"))
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView(tutors: testData)
+    }
+}
+#endif
+
+struct TutorCell : View {
+    let tutor: Tutor
+    var body: some View {
+        return NavigationLink(destination: TutorDetail(name: tutor.name, headline: tutor.headline, bio: tutor.bio)) {
+            Image(tutor.imageName)
+                .cornerRadius(40)
+            VStack(alignment: .leading) {
+                Text(tutor.name)
+                Text(tutor.headline)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+    }
+}
---
+++ TutorDetail.swift
@@ -0,0 +1,36 @@
+
+import SwiftUI
+
+struct TutorDetail : View {
+    var name: String
+    var headline: String
+    var bio: String
+    
+    var body: some View {
+        VStack {
+            Image(name)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.orange, lineWidth: 4)
+                )
+                .shadow(radius: 10)
+            Text(name)
+                .font(.title)
+            Text(headline)
+                .font(.subheadline)
+            Divider()
+            Text(bio)
+                .font(.headline)
+                .multilineTextAlignment(.center)
+                .lineLimit(50)
+            }.padding().navigationBarTitle(Text(name), displayMode: .inline)
+    }
+}
+
+#if DEBUG
+struct TutorDetail_Previews : PreviewProvider {
+    static var previews: some View {
+        TutorDetail(name: "Simon Ng", headline: "Founder of AppCoda", bio: "Founder of AppCoda. Author of multiple iOS prgramming books including Beginning iOS 12 Programming with Swift and Intermediate iOS 12 Programming with Swift. iOS Developer and Blogger.")
+    }
+}
+#endif
